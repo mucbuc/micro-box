6a4ea42a14443c14d434fb9546b205b15efc65f3
markymark <smallitle@hotmail.com>
2014-11-24 22:17:01 -0800

    added stack

--- stack.js
+++ stack.js
@@ -1,7 +1,8 @@
 var CD_Agent = require( 'cd-agent' )
   , AppStack = require( 'app-stack' )
   , logger = require( './logger.js')
-  , splitargs = require( 'splitargs' );
+  , splitargs = require( 'splitargs' )
+  , cp = require( 'child_process' );
 
 function Stack(controller) {
   var app = new AppStack( controller )
@@ -10,17 +11,41 @@ function Stack(controller) {
     , context = [];
 
   app.use( split );
-  app.use( logger );
+  //app.use( logger );
   app.use( /cd\s+.*/, changeDir );
+  app.use( function(params, res) {
+    if (!res.hasOwnProperty('purpose')) 
+      execute(params, res);
+  } ); 
 
   this.request = app.request;
 
+  function execute(params, res) {
+    var args = res.argv.length > 1 ? res.argv.splice(1) : []
+      , child;
+    
+    process.stdin.pause(); 
+    process.stdin.setRawMode( false );
+
+    child = cp.spawn( 
+      res.argv[0], 
+      args, 
+      { stdio: 'inherit' } 
+    );  
+        // process.stdout.on( 'data', function( data ) {
+    //  controller.emit( 'data', data );
+    // } );
+    //child.stdout.pipe( controller );
+    //child.stdout.pipe( process.stdout );
+    //process.stdout.pause(); 
+  }
+   
   function split(params, res) {
     res.argv = splitargs(params);
     res.end();
   }
 
-  function changeDir(params, req) {
+  function changeDir(params, res) {
     controller.once( 'cwd', function(next) {
       cwd = next;
       console.log( 'cwd: ', cwd );
@@ -30,7 +55,9 @@ function Stack(controller) {
       context = list;
     });
 
-    agent.process(req.argv, cwd);
+    agent.process(res.argv, cwd);
+    res.purpose = 'cd';
+    res.end();
   }
 }
 
