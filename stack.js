fed669aa537c3cb36ca21abe44fefc1103334046
markymark <smallitle@hotmail.com>
2014-11-28 05:33:53 -0800

    filter command

--- stack.js
+++ stack.js
@@ -2,7 +2,8 @@ var assert = require( 'assert' )
   , AppStack = require( 'app-stack' )
   , logger = require( './logger.js')
   , splitargs = require( 'splitargs' )
-  , cp = require( 'child_process' );
+  , cp = require( 'child_process' )
+  , config = require( './config.json' );
 
 function Stack(controller) {
   var app = new AppStack( controller );
@@ -24,23 +25,43 @@ function Stack(controller) {
       }
     }
 
-    process.stdin.pause(); 
-    process.stdin.setRawMode( false );
+    filterCommand( command, spawn, block );
 
-    child = cp.spawn( 
-      command, 
-      args, 
-      { 
-        stdio: 'inherit',
-        cwd: req.cwd
-      });
-
-    child.on( 'exit', function(code, signal) {
-      console.log( "code: ", code, "signal: ", signal );
-      process.stdin.resume();
-      process.stdin.setRawMode( true );
+    function block() {
+      var msg = "'" + command + "' is blocked\n"; 
+      controller.emit( 'feedback', msg );
       res.end();
-    }); 
+    }
+
+    function spawn() {
+      process.stdin.pause(); 
+      process.stdin.setRawMode( false );
+
+      child = cp.spawn( 
+        command, 
+        args, 
+        { 
+          stdio: 'inherit',
+          cwd: req.cwd
+        });
+
+      child.once( 'exit', function(code, signal) {
+        process.stdin.resume();
+        process.stdin.setRawMode( true );
+        res.end();
+      });
+    }
+
+    function filterCommand( command, pass, fail ) {
+      for (var r in config.sandbox) {
+        var re = new RegExp( config.sandbox[r] );
+        if (re.test( command )) {
+          pass();
+          return;
+        }
+      }
+      fail();
+    }
   }
    
   function split(req, res) {
