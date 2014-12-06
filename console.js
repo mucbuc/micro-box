bdf3d8c958a0b08f00d9802b52e58155c0a9945e
markymark <smallitle@hotmail.com>
2014-12-06 01:28:08 -0800

    cwd

--- console.js
+++ console.js
@@ -1,42 +1,95 @@
 #!/usr/bin/env node
 
-var readline = require( 'readline' );
+var assert = require( 'assert' )
+  , readline = require( 'readline' )
+  , Stack = require( './stack' );
 
 function Console() {
+
+  var writer = process.stdout.write
+    , outBuffer = ''
+    , stack = new Stack( controller )
+    , context
+    , cwd; 
+
+  stack.request( { params: 'cd' }, function(req, res) {
+    if (res.hasOwnProperty('context')) {
+      context = res.context; 
+      cwd = res.cwd;
+    }
+  } );
+  
+  process.stdout.write = function() {
+    outBuffer += arguments[0].toString();
+    return writer.apply(this, arguments); 
+  };
 
   var rl = readline.createInterface( { 
     input: process.stdin, 
     output: process.stdout,
     completer: function(partial, callback) {
-      var options = []
-        , ind = getBeginIndex( partial )
-            , options = []
-            , end = partial.substr( ind + 1 );
+      
+      assert( outBuffer.length ); 
 
-      stack.context.forEach( function( e, index, array ) {
+      var options = []
+        , ind = getBeginIndex( outBuffer )
+        , options = []
+        , end = outBuffer.substr( ind + 1 );
+
+      context.forEach( function( e, index, array ) {
             if (e.indexOf(end) == 0) {
               options.push( e );
             }
 
             if (index === array.length - 1) {
+              
+              if (options.length === 1) {
+                cwd += options[0];
+              }
               callback(null, [options, end] );
             }
       } );
 
+      if (!context.length) {
+        callback(null, [[], end] );
+      }
+      
       function getBeginIndex( command ) {
-          var a = command.lastIndexOf( ' ' )
-              , b = command.lastIndexOf( '/' );
+        var a = command.lastIndexOf( ' ' )
+          , b = command.lastIndexOf( '/' );
 
-            return b > a ? b : a;
+          return b > a ? b : a;
         }
       }
   });
 
   rl.on( 'line', function(cmd) {
+    
+    //console.log( outBuffer );
+
     stack.request( { 
       params: cmd 
     }, 
     read );
+  });
+
+  process.stdin.on( 'keypress', function( ch, key ) {
+    if (ch === '/') {
+      console.log( cwd );
+      // var lookAhead = 'node_modules' // outBuffer.substr( outBuffer.lastIndexOf( ' ' ) + 1, -1 )
+      //   , dir = cwd + '/' + lookAhead
+      //   , list = '';
+
+      // controller.on( 'feedback', accum ); 
+      // stack.request( { params: 'ls ' + dir }, function(req, res) {
+      //   controller.removeListener( 'feedback', accum );
+      //   context = list.split( '\n' );
+      // } );
+
+      // function accum( data ) {
+      //   list += data;
+      // }
+    }
   });
 
   read();
@@ -50,22 +103,12 @@ function Console() {
 if (!module.parent) {
   var events = require( 'events' )
     , controller = new events.EventEmitter()
-    , Stack = require( './stack' )
-    , stack = new Stack( controller )
     , c = new Console( controller );
-
-  stack.request( { params: 'cd' }, function(req, res) {
-    if (res.hasOwnProperty('context')) {
-      stack.context = res.context; 
-    }
-  } );
 
   controller.on( 'feedback', function(msg) {
     console.log( msg );
   });
-
-
-
 }
+
 module.exports = Console; 
 
