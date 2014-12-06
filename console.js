b46e558b70d228a95cc2c8c4259ccfe69b10aa66
markymark <smallitle@hotmail.com>
2014-12-05 21:02:29 -0800

    some improvements

--- console.js
+++ console.js
@@ -4,65 +4,65 @@ var readline = require( 'readline' );
 
 function Console() {
 
-	var rl = readline.createInterface( { 
-		input: process.stdin, 
-		output: process.stdout,
-		completer: function(partial, callback) {
-			var options = []
-			  , ind = getBeginIndex( partial )
-	          , options = []
-	          , end = partial.substr( ind + 1 );
+  var rl = readline.createInterface( { 
+    input: process.stdin, 
+    output: process.stdout,
+    completer: function(partial, callback) {
+      var options = []
+        , ind = getBeginIndex( partial )
+            , options = []
+            , end = partial.substr( ind + 1 );
 
-			stack.context.forEach( function( e, index, array ) {
-		        if (e.indexOf(end) == 0) {
-		        	options.push( e );
-		        }
+      stack.context.forEach( function( e, index, array ) {
+            if (e.indexOf(end) == 0) {
+              options.push( e );
+            }
 
-		        if (index === array.length - 1) {
-		        	callback(null, [options, end] );
-		        }
-			} );
+            if (index === array.length - 1) {
+              callback(null, [options, end] );
+            }
+      } );
 
-			function getBeginIndex( command ) {
-		    	var a = command.lastIndexOf( ' ' )
-		          , b = command.lastIndexOf( '/' );
+      function getBeginIndex( command ) {
+          var a = command.lastIndexOf( ' ' )
+              , b = command.lastIndexOf( '/' );
 
-		      	return b > a ? b : a;
-		    }
-  		}
-	});
+            return b > a ? b : a;
+        }
+      }
+  });
 
-	rl.on( 'line', function(cmd) {
-		stack.request( { 
-			params: cmd 
-		}, 
-		read );
-	});
+  rl.on( 'line', function(cmd) {
+    stack.request( { 
+      params: cmd 
+    }, 
+    read );
+  });
 
-	read();
+  read();
 
-	function read() {
-		rl.prompt( 'mb> ' );
-		rl.resume();
-	}
+  function read() {
+    rl.setPrompt( stack.cwd + '> ' );
+    rl.prompt();
+  }
 }
 
 if (!module.parent) {
-	var events = require( 'events' )
-	  , controller = new events.EventEmitter()
-	  , Stack = require( './stack' )
-	  , stack = new Stack( controller )
-	  , c = new Console( controller );
+  var events = require( 'events' )
+    , controller = new events.EventEmitter()
+    , Stack = require( './stack' )
+    , stack = new Stack( controller )
+    , c = new Console( controller );
 
-	stack.request( { params: 'cd' }, function(req, res) {
-		if (res.hasOwnProperty('context')) {
-			stack.context = res.context; 
-		}
-	} );
+  stack.request( { params: 'cd' }, function(req, res) {
+    if (res.hasOwnProperty('context')) {
+      stack.context = res.context; 
+    }
+  } );
 
-	controller.on( 'feedback', function(msg) {
-		console.log( msg );
-	});
+  controller.on( 'feedback', function(msg) {
+    console.log( msg );
+  });
 
 
 
--- stack.js
+++ stack.js
@@ -6,21 +6,33 @@ function Stack(controller) {
   var app = new AppStack( function() { 
         return { controller: controller }; 
       } )
-    , currentWorkingDir = process.cwd(); 
+    , currentWorkingDir = process.cwd()
+    , context = [];
+
+  this.__defineGetter__('context', function() {
+    return context;
+  });
+
+  this.__defineGetter__('cwd', function() { 
+    return currentWorkingDir;
+  });
 
   app.use( Layers.split );
   app.use( /cd\s*.*/, function(req, res) {
     cdAgent({ 
-        argv: req.argv 
+        argv: req.argv,
+        cwd: currentWorkingDir
       }, 
       function(cwd, list) {
         delete req.argv;
+        
         if (typeof cwd !== 'undefined') {
           res.cwd = cwd;
           currentWorkingDir = cwd;
         }
         if (typeof list !== 'undefined') {
           res.context = list;
+          context = list;
         }
         res.end();
     });
