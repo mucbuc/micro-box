f804a415d8539fbfcd11b24079f84747867b093d
markymark <smallitle@hotmail.com>
2014-12-15 06:44:48 -0800

    some cleanup

--- console.js
+++ console.js
@@ -12,10 +12,10 @@ console.log( macros );
 
 function Console() {
 
-  var writer = process.stdout.write
-    , stack = new Stack( controller )
+  var stack = new Stack( controller )
     , context
     , cwd
+    , rl
     , repeat = '';
 
   stack.request( { params: 'cd' }, function(req, res) {
@@ -25,58 +25,11 @@ function Console() {
     }
   } );
 
-  var rl = readline.createInterface( { 
+  rl = readline.createInterface( { 
     input: process.stdin, 
     output: process.stdout,
-    completer: function(partial, callback) {
-
-      var done = false;
-
-      assert( partial.length ); 
-
-      for(var property in macros) {
-        var macro = macros[property];
-        if (!partial.indexOf(property)) { 
-          callback(null, [ [property + macro], partial ] );
-          return;
-        }
-      }
-
-      if (!context.length) {
-        callback(null, [[], end] );
-      }
-      else {
-        searchContext();
-      } 
-
-      function searchContext() {
-
-        var options = []
-          , ind = getBeginIndex( partial )
-          , end = partial.substr( ind + 1 );
-
-        context.forEach( function( e, index, array ) {
-          if (e.indexOf(end) == 0) {
-            options.push( e );
-          }
-
-          if (index === array.length - 1) {
-            
-            if (options.length === 1) {
-              cwd += '/' + options[0];
-            }
-            callback(null, [options, end] );
-          }
-        } );
-      }
-
-      function getBeginIndex( command ) {
-        var a = command.lastIndexOf( ' ' )
-          , b = command.lastIndexOf( '/' );
-          return b > a ? b : a;
-        }
-      }
-  });
+    completer: complete
+  );
 
   rl.on( 'line', function(cmd) {
     stack.request( { 
@@ -86,7 +39,6 @@ function Console() {
   });
 
   process.stdin.on( 'keypress', function( ch, key ) {
-
     if (ch === '/') {
       stack.readdir( cwd, function(cwd, list) {
         context = list;
@@ -105,6 +57,56 @@ function Console() {
       rl.write( res.repeat );
     }
   }
+
+  function complete(partial, callback) {
+
+    var done = false;
+    assert( partial.length ); 
+
+    for(var property in macros) {
+      var macro = macros[property];
+      if (!partial.indexOf(property)) { 
+        callback(null, [ [property + macro], partial ] );
+        return;
+      }
+    }
+
+    if (!context.length) {
+      callback(null, [[], end] );
+    }
+    else {
+      searchContext();
+    } 
+
+    function searchContext() {
+
+      var options = []
+        , ind = getBeginIndex( partial )
+        , end = partial.substr( ind + 1 );
+
+      context.forEach( function( e, index, array ) {
+        if (e.indexOf(end) == 0) {
+          options.push( e );
+        }
+
+        if (index === array.length - 1) {
+          
+          if (options.length === 1) {
+            cwd += '/' + options[0];
+          }
+          callback(null, [options, end] );
+        }
+      } );
+    }
+
+    function getBeginIndex( command ) {
+      var a = command.lastIndexOf( ' ' )
+        , b = command.lastIndexOf( '/' );
+        return b > a ? b : a;
+      }
+    }
+  }
+
 }
 
 if (!module.parent) {
