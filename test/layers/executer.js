a31a0f0936f7a3dfb585553112d34eec08db959e
mbusenitz <mbusenitz@gmail.com>
2015-05-04 00:01:01 -0700

    master formatting

--- test/layers/executer.js
+++ test/layers/executer.js
@@ -12,82 +12,82 @@ assert( typeof Executer === 'function' );
 
 suite( 'executer', function() {
 
-	var executer
-	  , expector;
+  var executer
+    , expector;
 
-	setup(function() {
-		executer = new Executer();
-		expector = new Expector();
-	});
+  setup(function() {
+    executer = new Executer();
+    expector = new Expector();
+  });
 
-	teardown(function() {
-		expector.check();
-	});
+  teardown(function() {
+    expector.check();
+  });
 
-	test( 'checkError', function(done) {
-	  	
-		var context = defaultContext(done); 
-		context.exec = [['cat', 'doesNotExist.txt']];
-		context.cwd = __dirname;
+  test( 'checkError', function(done) {
+      
+    var context = defaultContext(done); 
+    context.exec = [['cat', 'doesNotExist.txt']];
+    context.cwd = __dirname;
 
-		expector.expectNot( 'stdout data' );
-		expector.expect( 'exit', { code: 1, signal: null } );
-		expector.expect( 'stderr data' );
+    expector.expectNot( 'stdout data' );
+    expector.expect( 'exit', { code: 1, signal: null } );
+    expector.expect( 'stderr data' );
 
-		executer.handle( context );
-	});
+    executer.handle( context );
+  });
 
-	test( 'checkOut', function(done) {
-		
-		var context = defaultContext(done);
-		context.exec = [['ls']];
-		context.cwd = path.join( __dirname, '../sample' );
-		
-		expector.expectNot( 'stderr data' );
-		expector.expect( 'stdout data', 'test_dummy.txt' );
-		expector.expect( 'exit', { code: 0, signal: null } );	
-		executer.handle( context );
-	});
+  test( 'checkOut', function(done) {
+    
+    var context = defaultContext(done);
+    context.exec = [['ls']];
+    context.cwd = path.join( __dirname, '../sample' );
+    
+    expector.expectNot( 'stderr data' );
+    expector.expect( 'stdout data', 'test_dummy.txt' );
+    expector.expect( 'exit', { code: 0, signal: null } ); 
+    executer.handle( context );
+  });
 
-	function defaultContext(done) {
-		return {
-			controller: expector, 
-			stdout: 'pipe',
-			stderr: 'pipe',
-			next: function(o) {
-				done();
-			}
-		};
-	}
+  function defaultContext(done) {
+    return {
+      controller: expector, 
+      stdout: 'pipe',
+      stderr: 'pipe',
+      next: function(o) {
+        done();
+      }
+    };
+  }
 
 });
 
 /* from jsthree test_processor.js
 function checkKill() {
-	var e = getDummy();
+  var e = getDummy();
 
-	e.on( 'exit', function() { 
-		console.log( 'check kill passed' );
-	} );
+  e.on( 'exit', function() { 
+    console.log( 'check kill passed' );
+  } );
 
-	e.emit( 'execute' );
-	e.emit( 'kill' );	
+  e.emit( 'execute' );
+  e.emit( 'kill' ); 
 }
 
 function checkIn() {
-	var e = getDummy();
+  var e = getDummy();
 
   e.emit( 'execute' );
   e.emit( 'stdin', 'a\n' );
   e.on( 'exit', function() { 
-  	console.log( 'check in passed' );
+    console.log( 'check in passed' );
   } );
 }
 
 function getDummy() {
-	var e = new events.EventEmitter()
-		, wd = path.join( __dirname, 'bin' )
-	  , p = new Processor( { cmd: "./dummy_read", cwd: wd }, e );
+  var e = new events.EventEmitter()
+    , wd = path.join( __dirname, 'bin' )
+    , p = new Processor( { cmd: "./dummy_read", cwd: wd }, e );
 
-	return e;
+  return e;
 }*/
