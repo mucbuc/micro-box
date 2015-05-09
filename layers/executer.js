683ab40dbfdc21bc9cddbd3a572be14f520f5f6e
mbusenitz <mbusenitz@gmail.com>
2015-05-09 14:06:33 -0700

    update

--- layers/executer.js
+++ layers/executer.js
@@ -14,7 +14,8 @@ function Executer() {
 
     if (o.hasOwnProperty('exec') && o.exec.length) {
 
-      var controller = new events.EventEmitter();
+      var controller = new events.EventEmitter()
+        , child;
 
       process.stdin.pause(); 
       process.stdin.setRawMode( false );
@@ -35,40 +36,14 @@ function Executer() {
         }
       });
 
-      execLine();
-
-      function execLine(outPath) {
-        
-        var streams = {}
-          , paths = {};
-
-        assert( o.exec.length );
-
-        controller.once( 'stdin ready', function(stdin) {
-          streams.stdin = stdin;
-          checkIfReady(streams);
-        } );
-
-        controller.once( 'stdout ready', function(stdout, path) {
-          streams.stdout = stdout;
-          streams.outPath = path;
-          checkIfReady(streams);
-        }); 
-
-        controller.once( 'stderr ready', function(stderr, path) {
-          streams.stderr = stderr;
-          streams.errPath = path;
-          checkIfReady(streams);
-        }); 
-
-        controller.once( 'ready', function(){
-          var child = spawn( getContext(
-                o, 
-                streams.stdin, 
-                streams.stdout, 
-                streams.stderr
-              ) );
-
+      child = mule( 
+        o.exec, 
+        { 
+          stdin: 'pipe', 
+          stderr: 'pipe'//,
+          //cwd: o.req.cwd
+        }, 
+        function(child) {
           if (child.stdout) {
             child.stdout.on( 'data', function(data){
               o.controller.emit( 'stdout data', data.toString() );
@@ -84,109 +59,19 @@ function Executer() {
           child.once( 'exit', function(code, signal) {
             controller.emit( 'exit', { 
                 code: code, 
-                signal: signal, 
-                outPath: paths.outPath, 
-                errPath: paths.errPath 
+                signal: signal
               });
           });
 
           o.controller.once( 'kill', function() {
             child.kill();
           }); 
-          
-          o.exec.splice(0,1);
-        });
-
-        openStdin(outPath, function(fd) {
-          controller.emit( 'stdin ready', fd );
-        });
-
-        openOutStream(o.exec, 'stdout', function(fd, path) {
-          controller.emit( 'stdout ready', fd, path );
-        });
-
-        openOutStream(o.exec, 'stderr', function(fd, path) {
-          controller.emit( 'stderr ready', fd, path );
-        });
-
-        function checkIfReady(streams) {
-          if(   streams.hasOwnProperty('stdin') 
-            &&  streams.hasOwnProperty('stdout')
-            &&  streams.hasOwnProperty( 'stderr' ))
-          {
-            controller.emit( 'ready' );  
-          }
-        }
-      }
+        }); 
     }
     else {
       o.next(o);
     }
-
-    function openOutStream(line, name, callback) {
-      if (line.length <= 1) {
-        callback( o.hasOwnProperty(name) ? o[name] : process[name] );
-      }
-      else {
-        tmp.file( function( err, path ) {
-          if (err) throw err;
-          fs.open(path, 'a+', function(err, fd) {
-            if (err) throw err;
-            callback(fd, path);
-          });
-        }); 
-      }
-    }
-
-    function openStdin(path, callback) {
-      if (typeof path === 'undefined') {
-        callback( process.stdin );
-      }
-      else {
-        fs.open(path, 'r', function(err, fd) {
-          if (err) throw err;
-          callback(fd);
-        });
-      }
-    }
-
-    function getContext(req, stdin, stdout, stderr) {
-      var args = req.exec[0]
-        , result = {
-          cmd: args[0],
-            argv: args.length > 1 ? args.splice(1) : [],
-            cwd: req.cwd,
-            stdin: stdin, 
-            stdout: stdout,
-            stderr: stderr
-          };
-      
-      return result;
-    }
   };
-
 }
-
-function spawn(context) {
-
-  assert(context.hasOwnProperty('cmd'));
-  assert(context.hasOwnProperty('cwd'));
-  assert(context.hasOwnProperty('argv')); 
-
-  ['stdin', 'stdout', 'stderr']
-  .forEach(function(name){
-    if (!context.hasOwnProperty(name)) {
-      context[name] = process[name];
-    }
-  });
-
-  return cp.spawn(
-    context.cmd, 
-    context.argv, 
-    { 
-      stdio: [ context.stdin, context.stdout, context.stderr ],
-      cwd: context.cwd
-    });
-} 
 
 module.exports = Executer;
