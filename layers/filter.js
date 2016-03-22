0a12406282f8bf274c5e0473847ca1f435b98817
mbusenitz <mbusenitz@gmail.com>
2016-03-22 06:31:09 -0700

    es6 and cleanup

--- layers/filter.js
+++ layers/filter.js
@@ -1,13 +1,14 @@
-var assert = require( 'assert' ) 
+'use strict';
+
+let assert = require( 'assert' ) 
   , config = require( '../config.json' );  
 
 assert( typeof config !== 'undefined' );
 
 function Filter() {
   this.handle = function(o) {
-    var tmp; 
-    for (var r in config.sandbox) {
-      var re = new RegExp( config.sandbox[r] );
+    for (let r in config.sandbox) {
+      let re = new RegExp( config.sandbox[r] );
       if (re.test(o.input)) {
         o.next();
         return;
--- layers/history.js
+++ layers/history.js
@@ -1,17 +1,19 @@
-var assert = require( 'assert' )
+'use strict';
+
+let assert = require( 'assert' )
   , fs = require( 'fs' );
 
 function Tracker() {
 
-	var output = fs.createWriteStream('history.txt');
-	process.on( 'exit', function() {
-		output.end();
-	});
+  let output = fs.createWriteStream('history.txt');
+  process.on( 'exit', () => {
+    output.end();
+  });
 
   this.handle = function(o) {
     assert( o.input !== 'undefined' );
     output.write( o.output + '\n' );
-	o.next();	
+    o.next(); 
   };
 }
 
